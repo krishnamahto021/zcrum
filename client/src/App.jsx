f751872ca5e4a25fecff7722e05ca0a1f17e4d73
unknown <mahtok98212@gmail.com>
2024-10-31 10:30:32 +0530

    make the header and setup the theme for light and dark

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { ThemeProvider } from "./components/theme-provider";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="zcrum-ui-theme">
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 };
 
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -5,16 +5,38 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { PenBox } from "lucide-react";
 
 const Header = () => {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link to={"/"}>
+          <img
+            src="https://krishnabucket021.s3.ap-southeast-2.amazonaws.com/zcrum/logo2.png"
+            loading="lazy"
+            className="h-10 w-auto object-contain"
+          ></img>
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive" className="flex items-center gap-3">
+              <PenBox size={16} />
+              <span className="hidden md:inline">Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
     </header>
   );
 };
